41bb82024b924c5bdaf20405c755420cf2505eba
Reet jirety <reetjirety@Reets-MacBook-Air.local>
2025-06-24 09:56:56 +0530

    auth routes created

---
+++ components/FormField.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const FormField = () => (
+    // <FormField
+    //                         control={form.control}
+    //                         name="username"
+    //                         render={({ field }) => (
+    //                             <FormItem>
+    //                                 <FormLabel>Username</FormLabel>
+    //                                 <FormControl>
+    //                                     <Input placeholder="shadcn" {...field} />
+    //                                 </FormControl>
+    //                                 <FormDescription>
+    //                                     This is your public display name.
+    //                                 </FormDescription>
+    //                                 <FormMessage />
+    //                             </FormItem>
+    //                         )}
+    //                     />
+    <p>form</p>
+    
+);
+
+export default FormField
